5f6987698ccf8b71df31861a978689188653561f
Reykudo <reykudo@DESKTOP-ECEDB2T.localdomain>
2020-07-08 23:33:48 +0300

    Init

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    run().await;
+}
+
+async fn run() {
+    teloxide::enable_logging!();
+    log::info!("Starting ping_pong_bot!");
+
+    let bot = Bot::from_env();
+
+    Dispatcher::new(bot)
+        .messages_handler(|rx: DispatcherHandlerRx<Message>| {
+            rx.for_each(|message| async move {
+                message.answer("text").send().await.log_on_error().await
+            })
+        })
+        .dispatch()
+        .await;
+}
